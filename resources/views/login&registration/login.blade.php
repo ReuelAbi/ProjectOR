baaa941f89decda95eb4d39e2f6b4acc9befeecf
ReuelAbi <44349138+ReuelAbi@users.noreply.github.com>
2021-03-02 22:17:24 -0700

    Testing new Login

--- resources/views/login&registration/login.blade.php
+++ resources/views/login&registration/login.blade.php
@@ -1,28 +1,27 @@
-@extends('layouts.master')
+@extends('layouts.master') 
 @section('title', 'Login Page')
 
 @section('content')
-		<form action = "signin" method = "POST">
-			<input type = "hidden" name = "_token" value = "<?php echo csrf_token()?>"/>
-			<h2>Login</h2>
-			<table>
-				<tr>
-					<td>Username: </td>
-					<td><input type = "text" name = "username" /></td>
-				</tr>
-				
-				<tr>
-					<td>Password: </td>
-					<td><input type = "password" name = "password" /></td>
-				</tr>
-				
-				<tr>
-					<td colspan = "2" align = "center">
-						<input type = "submit" value = "Login" />
-					</td>
-				</tr>
-			</table>
-		</form>
-		
-		<a href="registration">Register Here</a>
+<form action="signin" method="POST">
+	<input type="hidden" name="_token" value="<?php echo csrf_token()?>" />
+	<h2>Login</h2>
+	<table>
+		<tr>
+			<td>Username:</td>
+			<td><input type="text" name="username" /></td>
+		</tr>
+
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password" /></td>
+		</tr>
+
+		<tr>
+			<td colspan="2" align="center"><input type="submit" value="Login" />
+			</td>
+		</tr>
+	</table>
+</form>
+
+<a href="registration">Register Here</a>
 @endsection
